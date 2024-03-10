cb8579de23c18f33eed53f14688ad64110c064be
adammmusial <musialadam83@gmail.com>
2024-03-10 20:15:46 +0800

    feat: add event listeners

--- js/index.js
+++ js/index.js
@@ -1,25 +1,33 @@
 const display = document.querySelector(".display")
 const buttons = document.querySelectorAll(".button")
-const specialChars = ["%","*","/","0","="]
+const specialChars = ["%", "*", "/", "0", "="]
 let output = "";
 let historyDisplay = document.querySelector(".history")
 
 
 
 const calculate = (btnValue) => {
-    display.focus();  
+    console.log("Button clicked:", btnValue);
+    display.focus();
 
-    if (btnValue === "=" && output !== "") {  
+    if (btnValue === "=" && output !== "") {
         historyDisplay.innerHTML = output;
         output = eval(output.replace("%", "/100"));
     } else if (btnValue === "AC") {
         output = "0";
         historyDisplay.innerHTML = " ";
     } else if (btnValue === "DEL") {
-        output = output.slice(0, -1);  
+        output = output.slice(0, -1);
     } else {
-        if (output === "" && specialChars.includes(btnValue)) return;  
+        if (output === "" && specialChars.includes(btnValue)) return;
 
         output += btnValue;
     }
+    display.value = output;
 }
+
+
+for (let button of buttons) {
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+}
+
