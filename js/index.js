9a89b44e6c0e4d5d315a5ef17636ac9b8e12eeef
adammmusial <musialadam83@gmail.com>
2024-03-10 19:30:44 +0800

    feat: set variables

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const display = document.querySelector(".display")
+const buttons = document.querySelectorAll(".button")
+const specialChars = ["%","*","/","0","="]
+
+
+let output = "";
+let historyDisplay = document.querySelector(".history")
+
