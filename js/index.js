7a456bce95d26dd0717e8da94c49055f0c9c4c0b
adammmusial <musialadam83@gmail.com>
2024-03-10 19:48:26 +0800

    feat: add calculate function

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
 const display = document.querySelector(".display")
 const buttons = document.querySelectorAll(".button")
 const specialChars = ["%","*","/","0","="]
-
-
 let output = "";
 let historyDisplay = document.querySelector(".history")
 
+
+
+const calculate = (btnValue) => {
+    display.focus();  
+
+    if (btnValue === "=" && output !== "") {  
+        historyDisplay.innerHTML = output;
+        output = eval(output.replace("%", "/100"));
+    } else if (btnValue === "AC") {
+        output = "0";
+        historyDisplay.innerHTML = " ";
+    } else if (btnValue === "DEL") {
+        output = output.slice(0, -1);  
+    } else {
+        if (output === "" && specialChars.includes(btnValue)) return;  
+
+        output += btnValue;
+    }
+}
